d9af7aea99041a2ee2fef4812548b58bc7791df7
ClemDev2000 <65172423+ClemDev2000@users.noreply.github.com>
2022-10-31 14:33:20 +0100

    update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {
-      opacity: ['disabled'],
-    },
-  },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 };
